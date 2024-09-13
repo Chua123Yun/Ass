2ad9196fca51181003853f4b375324cd989bf933
Aki0626 <ernestko0626@gmail.com>
2024-09-14 01:06:15 +0800

    Update AppNavigator.tsx

--- AssFolder/AppNavigator.tsx
+++ AssFolder/AppNavigator.tsx
@@ -2,9 +2,8 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './Home Screen';
-
+import AdminLoginScreen from './AdminLoginScreen';
 import CreateScreen from './CreateScreen';
-
 import EditScreen from './EditScreen';
 import DirectoryScreen from './DirectoryScreen';
 import { RootStackParamList } from "./App"; // Import types
@@ -14,9 +13,11 @@ const Stack = createStackNavigator();
 const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="DirectoryScreen">
+      
       <Stack.Screen name="DirectoryScreen" component={DirectoryScreen} />
       <Stack.Screen name="CreateScreen" component={CreateScreen} />
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="AdminLoginScreen" component={AdminLoginScreen} />
       {/* Other screens can be added here */}
     </Stack.Navigator>
   );
